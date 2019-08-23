9800cf83334d4995f1f3c72cd633721b25534a5a
Jakub Makzlik <jakubmajzlik@gmail.com>
2019-08-23 21:21:44 +0200

    Add javadoc for Technique interface

--- src/main/java/sk/jakubmajzlik/sudokusolver/lib/technique/Technique.java
+++ src/main/java/sk/jakubmajzlik/sudokusolver/lib/technique/Technique.java
@@ -2,6 +2,17 @@ package sk.jakubmajzlik.sudokusolver.lib.technique;
 
 import sk.jakubmajzlik.sudokusolver.lib.GameGrid;
 
+/**
+ * Interface for solving techniques.
+ * @version 1.0
+ * @author Jakub Majzlík
+ */
 public interface Technique {
+    /**
+     * Applies technique to the {@code gameGrid} and returns game grid
+     * @param gameGrid Game grid
+     * @return Game grid with applied solving technique
+     * @since 1.0
+     */
     GameGrid apply(GameGrid gameGrid);
 }
