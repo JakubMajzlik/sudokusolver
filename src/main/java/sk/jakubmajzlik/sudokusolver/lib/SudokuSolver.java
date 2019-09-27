5b5b70b4ca115f37a2f849be647a409c625d62b3
Jakub Makzlik <jakubmajzlik@gmail.com>
2019-09-27 14:53:51 +0200

    Add missing doc comments

--- src/main/java/sk/jakubmajzlik/sudokusolver/lib/SudokuSolver.java
+++ src/main/java/sk/jakubmajzlik/sudokusolver/lib/SudokuSolver.java
@@ -5,8 +5,18 @@ import sk.jakubmajzlik.sudokusolver.lib.technique.SingleCandidate;
 import sk.jakubmajzlik.sudokusolver.lib.technique.SolvingTechnique;
 import sk.jakubmajzlik.sudokusolver.lib.technique.Technique;
 
+/**
+ * This class contains methods, which try to solve the game grid.
+ * @version 1.0
+ * @author Jakub Majzlík
+ */
 public class SudokuSolver {
 
+    /**
+     * Solves {@code gameGrid}. The method automatically choose algorithm to use for solving.
+     * @param gameGrid Game grid to solve.
+     * @since 1.0
+     */
     public void solve(GameGrid gameGrid) {
         SingleCandidate singleCandidate = new SingleCandidate();
         Scanning scanning = new Scanning();
@@ -18,6 +28,12 @@ public class SudokuSolver {
         } while (gameGrid.didProgress);
     }
 
+    /**
+     * Solves {@code gameGrid} with specific technique.
+     * @param solvingTechnique Technique, which we want to use.
+     * @param gameGrid Game grid to solve.
+     * @since 1.0
+     */
     public void solve(SolvingTechnique solvingTechnique, GameGrid gameGrid) {
         Technique technique;
         switch (solvingTechnique) {
