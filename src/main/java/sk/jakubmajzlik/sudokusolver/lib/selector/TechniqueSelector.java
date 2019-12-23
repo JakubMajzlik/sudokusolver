37919718720caf9907fd7b76b9669d51ffa296a4
Jakub Makzlik <jakubmajzlik@gmail.com>
2019-12-23 15:58:48 +0100

    Add interface for technique selection

---
+++ src/main/java/sk/jakubmajzlik/sudokusolver/lib/selector/TechniqueSelector.java
@@ -0,0 +1,16 @@
+package sk.jakubmajzlik.sudokusolver.lib.selector;
+
+import sk.jakubmajzlik.sudokusolver.lib.technique.Technique;
+
+/**
+ * Interface for algorithm selection
+ * @version 1.0
+ * @author Jakub Majzlík
+ */
+public interface TechniqueSelector {
+    /**
+     * Returns instance of {@code Technique} implementation.
+     * @return Technique 
+     */
+    Technique getTechnique();
+}
